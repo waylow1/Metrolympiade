0bcd9d5e9f2063112377d6b5c73f3d74f26e2bf1
TimAlbanUrl <mballu100@gmail.com>
2024-03-21 10:06:39 +0100

    build: navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { supabase } from '@/supabase'
 import { RouterLink, RouterView } from 'vue-router';
-import Navbar from '@/components/Navbar.vue'
+import Navbar from '@/components/NavbarComponent.vue'
 </script>
 <template>
   <Navbar></Navbar>
