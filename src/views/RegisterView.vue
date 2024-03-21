4f9d2b677faba94ef4d0a5a3c9f03ed1e45231e7
Cureau <baptiste.cureau@etu.unicaen.fr>
2024-03-21 10:14:27 +0100

    add redirection to menuView when sign up

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,47 +4,45 @@ import {supabase} from '@/supabase';
 import {useRouter} from 'vue-router'
 import AppButton from '@/components/AppButton.vue';
 
-const {push: routerPush} = useRouter();
-const email = ref('')
-const username = ref('')
-const password = ref('')
+const { push: routerPush } = useRouter();
+const email = ref('');
+const username = ref('');
+const password = ref('');
 
-const onSubmit = async()=>{
-    if(!email.value || !username.value || !password.value) return;
-   const {error} = await supabase.auth.signUp({
-        email: email.value,
-        password: password.value,
-        options:{
-            data:{
-                username:username.value
-            }
-        }
-    })
-    if(error) {
-        alert(error.message)
-    }else{
-        routerPush({name:'chat'})  
-    }
-}
-
+const onSubmit = async () => {
+  if (!email.value || !username.value || !password.value) return;
+  const { error } = await supabase.auth.signUp({
+    email: email.value,
+    password: password.value,
+  }, {
+    // Ici, vous pouvez ajouter les autres données utilisateur que vous souhaitez inclure
+    username: username.value,
+  });
+  if (error) {
+    console.error("Une erreur s'est produite lors de l'inscription :", error.message);
+  } else {
+    alert("L'inscription a été réussie");
+    routerPush({ name: 'menu' });
+  }
+};
 </script>
 
 <template>
-    <form @submit.prevent="onSubmit" class="flex flex-col gap-2 p-4 mx-auto max-w-96">
-      <div class="flex flex-col">
-        <label for="email">Email</label>
-        <input type="email" id="email" v-model="email" class="p-2 bg-slate-500"  required/>
-      </div>
-      <div class="flex flex-col">
-        <label for="username">Username</label>
-        <input type="text" id="username" v-model="username" class="p-2 bg-slate-500" required />
-      </div>
-      <div class="flex flex-col">
-        <label for="password">Mot de passe</label>
-        <input type="password" id="password" v-model="password" class="p-2 bg-slate-500" required/>
-      </div>
-      <AppButton>
-        Créer un compte
-      </AppButton>
-    </form>
-  </template>
+  <form @submit.prevent="onSubmit" class="flex flex-col gap-2 p-4 mx-auto max-w-96">
+    <div class="flex flex-col">
+      <label for="email">Email</label>
+      <input type="email" id="email" v-model="email" class="p-2 bg-slate-500" required/>
+    </div>
+    <div class="flex flex-col">
+      <label for="username">Username</label>
+      <input type="text" id="username" v-model="username" class="p-2 bg-slate-500" required />
+    </div>
+    <div class="flex flex-col">
+      <label for="password">Mot de passe</label>
+      <input type="password" id="password" v-model="password" class="p-2 bg-slate-500" required/>
+    </div>
+    <AppButton>
+      Let's goooo
+    </AppButton>
+  </form>
+</template>
