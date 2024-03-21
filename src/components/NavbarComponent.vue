9d9271282585d0405bc5caa9fb2e03af1f4a6d2d
TimAlbanUrl <mballu100@gmail.com>
2024-03-21 10:49:51 +0100

    build: login form

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -9,36 +9,15 @@ import { loadScript } from "vue-plugin-load-script";
     });
 </script>
 <template>
-  <div class="max-w-2xl mx-auto">
-    <nav class="border-gray-200 px-2 mb-10">
+  <div class="mx-auto bg-gray-900">
+    <nav class="p-2 border-gray-200 px-2 mb-10">
       <div class="container mx-auto flex flex-wrap items-center justify-between">
         <a href="#" class="flex">
-          <p class="text-blue-600 text-3xl font-black">M</p>
-          <span class="self-center text-lg font-semibold whitespace-nowrap">etrolymiade</span>
+          <p class="text-blue-700 text-3xl font-black">M</p>
+          <span class="self-center text-lg font-semibold whitespace-nowrap">etrolypmiade</span>
         </a>
         <div class="flex md:order-2">
-          <div class="relative mr-3 md:mr-0 hidden md:block">
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <svg
-                class="w-5 h-5 text-gray-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="email-adress-icon"
-              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2"
-              placeholder="Search..."
-            />
-          </div>
+          
           <button
             data-collapse-toggle="mobile-menu-3"
             type="button"
@@ -59,18 +38,7 @@ import { loadScript } from "vue-plugin-load-script";
                 clip-rule="evenodd"
               ></path>
             </svg>
-            <svg
-              class="hidden w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
+            
           </button>
         </div>
         <div
@@ -89,21 +57,21 @@ import { loadScript } from "vue-plugin-load-script";
             <li>
               <a
                 href="#"
-                class="text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0"
+                class="text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0"
                 >Ranking</a
               >
             </li>
             <li>
               <a
                 href="#"
-                class="text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0"
+                class="text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0"
                 >Match</a
               >
             </li>
             <li>
               <a
                 href="#"
-                class="text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0"
+                class="text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0"
                 >Logout</a
               >
             </li>
