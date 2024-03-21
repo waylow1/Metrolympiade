0a52c01fd2aec910cda3ddc679622e907ff8cfb0
Maxence <levesqu221@campus.unicaen.fr>
2024-03-21 10:58:38 +0100

    build: ajout des affichages

--- src/views/TeamSettingsView.vue
+++ src/views/TeamSettingsView.vue
@@ -2,10 +2,8 @@
 import {  supabase } from '@/supabase';
 
 
-
-
 const fetchTeamData = async() =>{
-    const { data, error } = await supabase.from('teams').select('*').eq('leader','edbdadfa-82ee-4757-a4b0-ab6e77dbe958');
+    const { data, error } = await supabase.from('teams').select('members,name').eq('leader','edbdadfa-82ee-4757-a4b0-ab6e77dbe958');
     if(error){
         console.log('error', error);
     }
@@ -14,14 +12,27 @@ const fetchTeamData = async() =>{
     }
 }
 
-const teamData = fetchTeamData();
+
+const teamData = await fetchTeamData();
+
+const membersList = teamData[0].members;
+
+const teamName = teamData[0].name;
+
+console.log(teamName);
 
 </script>
 
 <template>
         <div v-if="teamData">
             <label for="NomEquipe">Nom de l'équipe :</label>
-            <span> {{ teamData[""] }}</span>l
+            <span> {{ teamName }}</span>
+            <div :v-for="members in membersList">
+                <label for="NomMembre">Nom du membre :</label>
+                <span> {{ members["name"] }}</span>
+                <label for="EmailMembre">Email du membre :</label>
+                <span> {{ members["email"] }}</span>
+            </div>
         </div>
         <div v-else>
             <span>Chargement...</span>
