524b84da798cc40170bf3561eb8f8d594d84f7e1
tourbil221 <tourbil221@C304C-212705>
2024-03-21 10:36:45 +0100

    MatchCreation form

--- src/views/MatchCreationView.vue
+++ src/views/MatchCreationView.vue
@@ -2,36 +2,32 @@
 </script>
 
 <template>
-  <div class = "grid grid-cols-3">
-    <div>
-        <h1 class="col-start-2">Matchs</h1>
-    </div>
-    <form>
-        <div class="col-start-1 col-end-3s">
+   <div class="flex flex-col items-center justify-center">
+    <h1>Matchs</h1>
+    <form class="flex flex-col ">
+        <div class="space-y-6">
             <div>
-                <input type="text" class="border-2 rounded-md">
+                <input type="text" class="border-2 rounded-md text-center w-full">
             </div>
             <div>
-                <input type="select" class="border-2 rounded-md">
+                <select   class="border-2 rounded-md w-full text-center">
+                </select>
             </div>
             <div>
-                <input type="time" class="border-2 rounded-md">
+                <input type="time" class="border-2 rounded-md w-full text-center">
+            </div>
+        <div class="flex space-x-28">
+            <div class="flex flex-col">
+                <label for="myTeamScore">My team</label>
+                <input id="myTeamScore"  class="border-2 rounded-md size-16 text-center" type="number">
+            </div>
+            <div class="flex flex-col">
+                <label for="opponentTeamScore">Team 2</label>
+                <input id="opponentTeamScore" class="border-2 rounded-md size-16 text-center" type="number">
             </div>
         </div>
-    <div>
-        <div class="flex flex-col" >
-        <label for="myTeamScore">My team</label>
-        <input id="myTeamScore"  class="border-2 rounded-md" type="number">
-        </div>
-        <div class="flex flex-col">
-            <label for="opponentTeamScore">Team 2</label>
-            <input id="opponentTeamScore" class="border-2 rounded-md" type="number">
-        </div>
-        
-    </div>
-    <div class="col-start-2">
         <button type="submit" class="p-2 mt-4 bg-blue-500 rounded-md">Create the Match</button>
-    </div>
+      </div>
     </form>
-  </div>
+</div>
 </template>
