ccd32668ad94a199eb71224fc5002dadd3bfcd6f
tourbil221 <tourbil221@C304C-212705>
2024-03-21 10:07:07 +0100

    feat: MatchCreation form

--- src/views/MatchCreationView.vue
+++ src/views/MatchCreationView.vue
@@ -2,21 +2,36 @@
 </script>
 
 <template>
-  <div>
-    <h1>Matchs</h1>
+  <div class = "grid grid-cols-3">
     <div>
-        <input type="text" >
-        <input type="text">
-        <input type="time">
+        <h1 class="col-start-2">Matchs</h1>
     </div>
+    <form>
+        <div class="col-start-1 col-end-3s">
+            <div>
+                <input type="text" class="border-2 rounded-md">
+            </div>
+            <div>
+                <input type="select" class="border-2 rounded-md">
+            </div>
+            <div>
+                <input type="time" class="border-2 rounded-md">
+            </div>
+        </div>
     <div>
-        <p>My team</p>
-        <input type="number">
-        <p>Team 2</p>
-        <input type="number">
+        <div class="flex flex-col" >
+        <label for="myTeamScore">My team</label>
+        <input id="myTeamScore"  class="border-2 rounded-md" type="number">
+        </div>
+        <div class="flex flex-col">
+            <label for="opponentTeamScore">Team 2</label>
+            <input id="opponentTeamScore" class="border-2 rounded-md" type="number">
+        </div>
+        
     </div>
-    <div>
-        <button>Create the Match</button>
+    <div class="col-start-2">
+        <button type="submit" class="p-2 mt-4 bg-blue-500 rounded-md">Create the Match</button>
     </div>
+    </form>
   </div>
 </template>
