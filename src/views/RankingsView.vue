76f5bec1c849a7eb65177998fced3ea090fa7065
Maxence <levesqu221@campus.unicaen.fr>
2024-03-27 16:30:53 +0100

    !.

--- src/views/RankingsView.vue
+++ src/views/RankingsView.vue
@@ -1,8 +1,17 @@
 <script setup>
+    import {fetchPoints} from '@/api/points';
+    import {fetchTeams} from '@/api/teams';
+import { onMounted } from 'vue';
 
+
+    onMounted(async () => {
+        const points = await fetchPoints();
+        console.log(points);
+        const teams = await fetchTeams();
+        console.log(teams);
+    });
 
 </script>
-
 <template>
  <p>ca marche</p>
 </template>
