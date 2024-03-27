f8e68f21e85f4d41094963e23cb4d33c546eab48
Cureau <baptiste.cureau@etu.unicaen.fr>
2024-03-27 15:01:25 +0100

    add function logout

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,85 @@
+<script setup>
+import { loadScript } from "vue-plugin-load-script";
+import { useRouter } from 'vue-router';
+import { supabase } from '@/supabase';
+import { ref } from 'vue';
+
+
+const router = useRouter();
+const loading = ref(false);
+
+const logout = async ()=>{
+    loading.value = true;
+    await supabase.auth.signOut()
+    router.push('/login')
+}
+loadScript("https://unpkg.com/@themesberg/flowbite@1.1.1/dist/flowbite.bundle.js")
+  .then(() => {
+
+  })
+  .catch(() => {
+    console.log("failed loading external script")
+  });
+
+const navigate = (path) => {
+  router.push({ name: path });
+}
+</script>
+<template>
+  <div class="max-w-2xl mx-auto">
+    <nav class="border-gray-200 px-2 mb-10">
+      <div class="container mx-auto flex flex-wrap items-center justify-between">
+        <a href="#" class="flex">
+          <p class="text-blue-600 text-3xl font-black">M</p>
+          <span class="self-center text-lg font-semibold whitespace-nowrap">etrolymiade</span>
+        </a>
+        <div class="flex md:order-2">
+          <button data-collapse-toggle="mobile-menu-3" type="button"
+            class="md:hidden text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center"
+            aria-controls="mobile-menu-3" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"></path>
+            </svg>
+            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"></path>
+            </svg>
+          </button>
+        </div>
+        <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
+          <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+            <li>
+              <button type="button" @click="navigate('teamSettings')" class="bg-white hover:bg-white focus:outline-none">
+                <a href="#"
+                  class="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded"
+                  aria-current="page">Team settings</a>
+              </button>
+            </li>
+            <li>
+              <button type="button" @click="navigate('rankings')" class="bg-white hover:bg-white focus:outline-none">
+              <a href="#"
+                class="text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Ranking</a>
+              </button>
+            </li>
+            <li>
+              <button type="button" @click="navigate('matchs')" class="bg-white hover:bg-white focus:outline-none">
+              <a href="#"
+                class="text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Match</a>
+              </button>
+            </li>
+            <li>
+              <button  type="button" @click="logout" class="bg-white hover:bg-white focus:outline-none" :loading="loading">
+              <a href="#"
+                class="text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Logout</a>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
