58a7f61757447cceb9dbd46f19ca9b987e5b94c6
Orianne <orianne.essienth@etu.unicaen.fr>
2024-03-28 11:48:12 +0100

    try to fix matchs

--- src/views/MatchsView.vue
+++ src/views/MatchsView.vue
@@ -9,38 +9,45 @@ const matchList = ref([]);
 
 onMounted (async () => {
     matchList.value = await fetchMatchs()
+    showMatchs();
 })
 
  function showMatchs(){
     var matchs= document.getElementById("matchs");
-
-    matchList.array.forEach(match => {
+    var i =0;
+    matchList.value.forEach(match => {
+        i+=1;
         var div = document.createElement("div")
-
-        var team1 = match.team1;
+        div.id="match_"+i
+        div.className = "matchList"
+        div.appendChild(document.createElement("p"))
+        let team1 = match.team1;
         var team2 = match.team2;
         var team1_score = match.team1_score;
         var team2_score = match.team2_score;
         var time = match.time;
         var sport = match.sport;
-        div.appendChild(team1,team2,team1_score,team2_score,time,sport);
+        div.innerText+=team1;
+       /* div.appendChild(team1,team2,team1_score,team2_score,time,sport);*/
         matchs.appendChild(div);
+        console.log(match)
+        
     });
  }
 
- showMatchs();
+
 
 </script>
 <template>
     <h1 class="text-center p-5">
         Matchs
     </h1>
-    <div id="matchs" flex justify-center>
-        <div class = " border-2 p-3  w-1/2 mx-auto text-center rounded-md">
+    <div  flex justify-center>
+        <div id="matchs" class = " border-2 p-3  w-1/2 mx-auto text-center rounded-md">
             <!--<p>11H04 - Volleyball</p>
             <p>SpaceX vs Meta</p>
-            <p>8-10</p>
-            {{matchList}}-->
+            <p>8-10</p>-->
+            <!--{{matchList}}-->
         </div>
         <div class = "w-1/2 mx-auto text-center">
             <button class="m-5 border-2 border-green-500 rounded-md p-3"  @click=" routerPush({ name: 'matchCreation' });">Create a match</button>
