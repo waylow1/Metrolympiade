7b20dc07bc2c8a1427d4a98c3f71275e69b47f54
martin <22201275@etu.unicaen.fr>
2024-03-28 14:17:37 +0100

    fix: navbar routes

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -25,10 +25,10 @@ loadScript("https://unpkg.com/@themesberg/flowbite@1.1.1/dist/flowbite.bundle.js
   <div id="navbar" class="max-w-2xl mx-auto">
     <nav class="border-gray-200 px-2 mb-10">
       <div class="container mx-auto flex flex-wrap items-center justify-between">
-        <a href="#" class="flex md:hover:bg-gray-900">
+        <div class="flex md:hover:bg-gray-900">
           <p class="text-3xl font-black" style="color: #ffffff;">M</p>
           <span class="self-center text-lg font-semibold whitespace-nowrap" style="color: #ff7b00;">etrolympiade</span>
-        </a>
+        </div>
         <div class="flex md:order-2">
           <button data-collapse-toggle="mobile-menu-3" type="button"
             class="md:hidden text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center"
@@ -50,28 +50,24 @@ loadScript("https://unpkg.com/@themesberg/flowbite@1.1.1/dist/flowbite.bundle.js
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
             <li>
               <button type="button" @click="routerPush({ name: 'teamSettings' })">
-                <a href="#"
-                  class="hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-white md:p-0"
+                <p class="hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-white md:p-0"
                   style="color: #ff7b00;"
-                  aria-current="page">Team settings</a>
+                  aria-current="page">Team settings</p>
               </button>
             </li>
             <li>
               <button type="button" @click="routerPush({ name: 'rankings' })">
-              <a href="#"
-                class="text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-400 md:p-0">Ranking</a>
+              <p class="text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-400 md:p-0">Ranking</p>
               </button>
             </li>
             <li>
               <button type="button" @click="routerPush({ name: 'matchs' })">
-              <a href="#"
-                class="text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-400 md:p-0">Match</a>
+              <p class="text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-400 md:p-0">Match</p>
               </button>
             </li>
             <li>
               <button  type="button" @click="logout">
-              <a href="#"
-                class="text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-400 md:p-0">Logout</a>
+              <p class="text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-400 md:p-0">Logout</p>
               </button>
             </li>
           </ul>
